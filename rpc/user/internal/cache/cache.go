09529e1bcd048e1b05269d72112ca0d20709934c
AsakiRain <asak@irain.cc>
2023-09-05 12:46:53 +0800

    feat: IsFollow的redis查询

---
+++ rpc/user/internal/cache/cache.go
@@ -0,0 +1,26 @@
+package cache
+
+import "fmt"
+
+func GenFollowKey(userId int64, targetId int64) string {
+	return fmt.Sprintf("follow_%d_%d", userId, targetId)
+}
+
+func GenFollowingCountKey(userId int64) string {
+	return fmt.Sprintf("following_count_%d", userId)
+}
+
+func GenFollowerCountKey(userId int64) string {
+	return fmt.Sprintf("follower_count_%d", userId)
+}
+
+func GenUserInfoKey(userId int64) string {
+	return fmt.Sprintf("user_info_%d", userId)
+}
+
+func YesOrNo(cond bool) string {
+	if cond {
+		return "yes"
+	}
+	return "no"
+}
--- rpc/user/internal/logic/isFollowLogic.go
+++ rpc/user/internal/logic/isFollowLogic.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"tikstart/common/model"
 	"tikstart/common/utils"
-
+	"tikstart/rpc/user/internal/cache"
 	"tikstart/rpc/user/internal/svc"
 	"tikstart/rpc/user/user"
 
@@ -26,14 +26,36 @@ func NewIsFollowLogic(ctx context.Context, svcCtx *svc.ServiceContext) *IsFollow
 }
 
 func (l *IsFollowLogic) IsFollow(in *user.IsFollowRequest) (*user.IsFollowResponse, error) {
+	// cache miss with return ""
+	val, err := l.svcCtx.RDS.Get(cache.GenFollowKey(in.UserId, in.TargetId))
+	if err != nil {
+		return nil, utils.InternalWithDetails("(redis)error getting follow relation", err)
+	}
+	if val != "" {
+		if val == "yes" {
+			return &user.IsFollowResponse{
+				IsFollow: true,
+			}, nil
+		} else {
+			return &user.IsFollowResponse{
+				IsFollow: false,
+			}, nil
+		}
+	}
+
 	var count int64
-	err := l.svcCtx.DB.
+	err = l.svcCtx.DB.
 		Model(&model.Follow{}).
 		Where("follower_id = ? AND followed_id = ?", in.UserId, in.TargetId).
 		Count(&count).
 		Error
 	if err != nil {
-		return nil, utils.InternalWithDetails("error querying follow relation", err)
+		return nil, utils.InternalWithDetails("(mysql)error querying follow relation", err)
+	}
+
+	err = l.svcCtx.RDS.Set(cache.GenFollowKey(in.UserId, in.TargetId), cache.YesOrNo(count == 1))
+	if err != nil {
+		logx.WithContext(l.ctx).Errorf("(redis)error setting follow relation", err)
 	}
 
 	return &user.IsFollowResponse{
